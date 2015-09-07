daf6bfec7bbe62e4c317b469dcb44e2a8f93d1b7
Sridhar Katakam <srikat@gmail.com>
2015-09-07 22:58:06 +1000

    Cleaned up comments formatting

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
-//* Start the engine
+// Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
-//* Child theme (do not remove)
+// Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.2.0' );
 
-//* Enqueue Google Fonts
+// Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
@@ -15,20 +15,20 @@ function genesis_sample_google_fonts() {
 
 }
 
-//* Add HTML5 markup structure
+// Add HTML5 markup structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
 
-//* Add Accessibility support
+// Add Accessibility support
 // add_theme_support( 'genesis-accessibility', array( 'headings', 'drop-down-menu', 'search-form', 'skip-links', 'rems' ) );
 add_theme_support( 'genesis-accessibility', array( 'headings', 'search-form', 'skip-links', 'rems' ) );
 
-//* Add viewport meta tag for mobile browsers
+// Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add support for custom background
+// Add support for custom background
 add_theme_support( 'custom-background' );
 
-//* Add support for 3-column footer widgets
+// Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
 
 /**********************************
